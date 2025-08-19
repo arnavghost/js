cf3b18462badbf3ada9e1c259ec24caa3a639dab
arnavghost <arnavkhandelwal1464@gmail.com>
2025-08-19 20:40:07 +0530

    Add examples of function creation and usage in JavaScript

---
+++ 03basics/01_functions.js
@@ -0,0 +1,22 @@
+// function name(){
+//     console.log("arnav");
+// }
+// name();
+
+function add(n1,n2){    //n1, n2 are parameters
+    // let result=(n1+n2);
+    // return result;
+    return (n1+n2); 
+}
+const result= add(4,5); // 4, 5 are arguments
+console.log(result);   // 9
+
+function logInUserMessage(username){
+    if(username === undefined){      //if(!username)
+        console.log("please enter a username");
+        return
+    }
+    return`${username} is logged in`;
+}
+console.log(logInUserMessage("Arnav"));
+console.log(logInUserMessage());// (undefined) is logged in
