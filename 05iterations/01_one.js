3d8dbfb2c474cac4bfe583cda8738ddc66be29e3
arnavghost <arnavkhandelwal1464@gmail.com>
2025-08-20 21:00:12 +0530

    Add examples of for, while, and do-while loops, including break and continue statements in JavaScript

---
+++ 05iterations/01_one.js
@@ -0,0 +1,29 @@
+//for
+
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    
+}
+
+// break and continue
+for (let index = 1; index < 20; index++) {
+    if (index === 5) {
+        console.log('Breaking the loop');
+        break; // continue
+    }
+    console.log(`value of i is ${index}`);
+}
+
+///while loop
+let index = 0;
+while (index < 10) {
+    // code block to be executed
+    console.log(`value of index is ${index}`);
+    index++;
+}
+
+// do while
+do {
+    
+} while (condition);
+
