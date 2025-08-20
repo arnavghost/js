66ff60b791243f1dc257e1bce94a2f0d3a40b884
arnavghost <arnavkhandelwal1464@gmail.com>
2025-08-20 21:00:30 +0530

    Add examples of forEach method for array iteration in JavaScript

---
+++ 05iterations/04_four.js
@@ -0,0 +1,41 @@
+const coding= ["java", "javascript", "python", "c++"];
+
+// coding.forEach( function (item) {
+//    console.log(item);
+// } )
+
+// coding.forEach( () => {
+//    console.log(item);
+// })
+
+// function print(item){
+//     console.log(item);
+// }
+// coding.forEach(print);
+
+
+// coding.forEach( (item,index,arr) => {
+//     console.log(item, index, arr);
+// })
+
+
+
+
+// const myCoding= [ 
+//     {
+//         languagename: "java",
+//         languagefilename: "java"
+//     },
+//     {
+//         languagename: "python",
+//         languagefilename: "py"
+//     },
+//     {
+//         languagename: "javascript",
+//         languagefilename: "js"
+//     },
+// ]
+
+// myCoding.forEach( (item) => {
+//     console.log(item.languagename);
+// })
